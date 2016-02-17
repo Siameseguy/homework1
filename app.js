4d035c7917c26fee6b3dc0ae5d5927befe0c2f78
siameseguy <psyhakhom@gmail.com>
2016-02-16 20:31:36 -0500

    started part 2 of homework

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ angular.module('myApp', [])
 .controller('mainCtrl', function(){
   var self = this;
  
+  self.listOne = [
+  	'1.1', '1.2', '1.3'
+  ]
+
+  self.listTwo = [
+  	'2.2', '2.3', '2.3'
+  ]
   
 })
